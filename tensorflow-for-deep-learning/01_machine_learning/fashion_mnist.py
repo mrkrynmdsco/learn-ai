2abccd8a5f6eb090af6fcffd9af2b8ff5472e641
Mark Dasco <mark.dasco@psware.com>
2019-10-07 10:18:56 +0800

    Trained Fashion MNIST

--- tensorflow-for-deep-learning/01_machine_learning/fashion_mnist.py
+++ tensorflow-for-deep-learning/01_machine_learning/fashion_mnist.py
@@ -63,3 +63,41 @@ plt.imshow(image, cmap=plt.cm.binary)
 plt.colorbar()
 plt.grid(False)
 plt.show()
+
+# display the first 25 images from test set with class name below each
+plt.figure(figsize=(10, 10))
+i = 0
+for (image, label) in test_dataset.take(25):
+    image = image.numpy().reshape((28, 28))
+    plt.subplot(5, 5, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(image, cmap=plt.cm.binary)
+    plt.xlabel(class_names[label])
+    i += 1
+plt.show()
+
+# Build the model
+# setup the layers
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28, 1)),  # input
+    tf.keras.layers.Dense(128, activation=tf.nn.relu),  # hidden
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)  # output
+])
+
+# compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model
+BATCH_SIZE = 32
+train_dataset = train_dataset.repeat().shuffle(num_train_examples).batch(BATCH_SIZE)
+test_dataset = test_dataset.batch(BATCH_SIZE)
+
+model.fit(train_dataset, epochs=5, steps_per_epoch=math.ceil(num_train_examples / BATCH_SIZE))
+
+# Evaluate accuracy
+test_loss, test_accuracy = model.evaluate(test_dataset, steps=math.ceil(num_test_examples / 32))
+print('\nAccuracy on test dataset:', test_accuracy)
